d30acda3955949f9900a35a0e9e20d9e4076dbcd
CChristopherL <150044796+CChristopherL@users.noreply.github.com>
2025-01-15 21:33:59 +0100

    Create led.c

---
+++ Src/led.c
@@ -0,0 +1,46 @@
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
+
+#include "led.h"
+
+void resetLED() {
+	// Reset red
+	GPIOB->OSPEEDR &= ~(0x00000003 << (4 * 2));
+	GPIOB->OTYPER &= ~(0x0001 << 4);
+	GPIOB->MODER &= ~(0x00000003 << (4 * 2));
+	GPIOB->ODR &= ~(0x00000003 << (4 * 2));
+
+	// Reset green
+	GPIOC->OSPEEDR &= ~(0x00000003 << (7 * 2));
+	GPIOC->OTYPER &= ~(0x0001 << 7);
+	GPIOC->MODER &= ~(0x00000003 << (7 * 2));
+	GPIOC->ODR &= ~(0x00000003 << (7 * 2));
+
+	// Reset blue
+	GPIOA->OSPEEDR &= ~(0x00000003 << (9 * 2));
+	GPIOA->OTYPER &= ~(0x0001 << 9);
+	GPIOA->MODER &= ~(0x00000003 << (9 * 2));
+	GPIOA->ODR &= ~(0x00000003 << (9 * 2));
+}
+
+
+void setRed() {
+	GPIOB->OSPEEDR |= (0x00000002 << (4 * 2));
+	GPIOB->OTYPER |= (0x0000 << 4);
+	GPIOB->MODER |= (0x00000001 << (4 * 2));
+	GPIOB->ODR |= (0x00000001 << (4 * 2));
+}
+
+void setGreen() {
+	GPIOC->OSPEEDR |= (0x00000002 << (7 * 2));
+	GPIOC->OTYPER |= (0x0000 << 7);
+	GPIOC->MODER |= (0x00000001 << (7 * 2));
+	GPIOC->ODR |= (0x00000001 << (7 * 2));
+}
+
+void setBlue() {
+	GPIOA->OSPEEDR |= (0x00000002 << (9 * 2));
+	GPIOA->OTYPER |= (0x0000 << 9);
+	GPIOA->MODER |= (0x00000001 << (9 * 2));
+	GPIOA->ODR |= (0x00000001 << (9 * 2));
+}
