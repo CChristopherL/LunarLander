9cfc0fcfd1e2e6d59dfefbc3a9027a8dd8571938
CChristopherL <150044796+CChristopherL@users.noreply.github.com>
2025-01-15 22:45:08 +0100

    Update renderer.h

--- Inc/renderer.h
+++ Inc/renderer.h
@@ -1,12 +1,12 @@
 #ifndef RENDERER_H_
 #define RENDERER_H_
 
-#define ESC 0x1B
-
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <stdint.h>
+
+#define ESC 0x1B
 
 void initDisplay(uint32_t *runtime);
 void** getBatch(uint8_t batch);
