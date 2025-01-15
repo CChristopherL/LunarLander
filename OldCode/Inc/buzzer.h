9e3bc4b953944c8a83ad2859eeadbcaad145f3fe
CChristopherL <150044796+CChristopherL@users.noreply.github.com>
2025-01-15 20:04:44 +0100

    Create buzzer.h

---
+++ OldCode/Inc/buzzer.h
@@ -0,0 +1,15 @@
+//to run "buzzer" you need: "timer"
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+#include "stm32f30x_conf.h"
+#include "30010_io.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "timer.h"
+void initBuzzer();
+void setFreq(uint16_t freq);
+
+#endif /* BUZZER_H_ */
