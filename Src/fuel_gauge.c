05c1a38153dc78b77fc9b8a6c29119db1e4f9471
CChristopherL <150044796+CChristopherL@users.noreply.github.com>
2025-01-15 22:22:21 +0100

    Create fuel_gauge.c

---
+++ Src/fuel_gauge.c
@@ -0,0 +1,47 @@
+#include "fuel_gauge.h"
+
+#include "led.h"
+
+void updateFuelGauge(uint32_t *runtime, uint16_t fuel) {
+	static uint8_t color = NONE;
+
+	// High fuel = green
+	if (fuel > 750) {
+		if (color != GREEN) {
+			resetLEDs();
+			setGreen();
+			color = GREEN;
+		}
+	}
+
+	// Mid fuel = yellow
+	else if (fuel > 250) {
+		if (color != YELLOW) {
+			resetLEDs();
+			//setRed();
+			//setGreen();
+			color = YELLOW;
+		}
+	}
+
+	// Low fuel = red
+	else if (fuel > 100) {
+		if (color != RED) {
+			resetLEDs();
+			setRed();
+			color = RED;
+		}
+	}
+
+	// Very low fuel = blinking red
+	else if (((*runtime >> 6) & 1)) {
+		if (color != NONE) {
+			resetLEDs();
+			color = NONE;
+		}
+	} else if (color != RED) {
+		resetLEDs();
+		setRed();
+		color = RED;
+	}
+}
